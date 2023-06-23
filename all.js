47126c0b02856ff10942600ecfd14100bbc2b369
DESKTOP-RP4BFD4\AKI <ahsi.aki@gmail.com>
2023-06-23 15:28:20 +0800

    feat: rwd menu

--- all.js
+++ all.js
@@ -1,5 +1,17 @@
-$(() => {
-  $(".ck-btn").click((e) => {
-    alert("clicked!");
+$(document).ready(function() {
+  $('.nav--item').click(function(e) {
+    e.preventDefault();
+    $('.nav--item--detail').hide();
+    var detail = $(this).find('.nav--item--detail');
+    detail.slideToggle();
   });
+
+  $(".footer--icons--top").click(function(){
+    e.preventDefault();
+    $("html,body").animate({ scrollTop: 0 }, 2000);
+  });
+
+  $(".nav--mobile--open").click(function(){
+    $(".nav--mobile").slideToggle();
+  })
 });
