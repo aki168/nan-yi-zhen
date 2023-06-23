a05af8b5c0570c3dc1db488c9da217f90d92bc6a
DESKTOP-RP4BFD4\AKI <ahsi.aki@gmail.com>
2023-06-23 18:09:05 +0800

    import swiper

--- all.js
+++ all.js
@@ -1,22 +1,44 @@
-$(document).ready(function() {
-  $('.nav--item').click(function(e) {
+$(document).ready(function () {
+  $(".nav--item").click(function (e) {
     e.preventDefault();
-    var detail = $(this).find('.nav--item--detail');
+    var detail = $(this).find(".nav--item--detail");
     detail.slideToggle();
   });
 
-  $('.nav--mobile .nav--item').click(function(e) {
+  $(".nav--mobile .nav--item").click(function (e) {
     e.preventDefault();
-    var detail = $(this).find('.nav--item--detail--mobile');
+    var detail = $(this).find(".nav--item--detail--mobile");
     detail.slideToggle();
   });
 
-  $(".footer--icons--top").click(function(){
+  $(".footer--icons--top").click(function () {
     e.preventDefault();
     $("html,body").animate({ scrollTop: 0 }, 2000);
   });
 
-  $(".nav--mobile--open").click(function(){
+  $(".nav--mobile--open").click(function () {
     $(".nav--mobile").slideToggle();
-  })
+  });
+});
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") prevSlide();
+  e.key === "ArrowRight" && nextSlide();
+});
+
+const swiper = new Swiper(".swiper", {
+  direction: "horizontal",
+  loop: true,
+  autoplay: {
+    delay: 5000,
+  },
+  // If we need pagination
+  // pagination: {
+  //   el: ".swiper-pagination",
+  // },
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
