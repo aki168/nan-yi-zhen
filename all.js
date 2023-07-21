85a6f5bc00f23f7af1638a0e5a570886e6eb7f5c
BECREATOR/aki.zheng <ahsi.aki@gmail.com>
2023-07-21 14:58:33 +0800

    reverse tab and pane

--- all.js
+++ all.js
@@ -20,11 +20,10 @@ $(document).ready(function () {
     $(this).siblings().find(".nav--item--detail--mobile").hide();
   });
 
-
-  $(".index--reserve--item").hover(function(e){
+  $(".index--reserve--item").hover(function (e) {
     var detail = $(this).find("span");
     detail.toggle();
-  })
+  });
 
   $(".footer--icons--top").click(function (e) {
     e.preventDefault();
@@ -34,6 +33,23 @@ $(document).ready(function () {
   $(".nav--mobile--open").click(function () {
     $(".nav--mobile").slideToggle();
   });
+});
+
+$(".reverse-nav li").click(function (e) {
+  $(this).addClass("border-b-yellow-800/50 text-yellow-800");
+  $(this).siblings().removeClass("border-b-yellow-800/50 text-yellow-800");
+  let currentId = e.currentTarget.id;
+  let panes = $(".reverse-pane").children();
+  if (panes) {
+    for (i = 0; i < panes.length; i++) {
+      if (panes[i].classList.contains(currentId)) {
+        panes[i].classList.remove("hidden");
+        panes[i].classList.add("block");
+      } else {
+        panes[i].classList.add("hidden");
+      }
+    }
+  }
 });
 
 const swiper = new Swiper(".swiper", {
